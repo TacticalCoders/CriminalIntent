9e771c37be8e3492ef4d3ffff3e641e780483b3b
Byeongha Kang <docdoca@naver.com>
2022-01-08 22:21:42 +0900

    chapter11(Room을 활용한 데이터베이스 구축)

--- app/src/main/java/com/biginerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/biginerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -3,13 +3,17 @@ package com.biginerdranch.android.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() { //데이터를 장기간 저장할 수 있는 저장소는 아니다.(장기간 데이터 처리는 11장에서)
-    val crimes = mutableListOf<Crime>()
-    init {
+    //val crimes = mutableListOf<Crime>()
+
+    /*init {
         for(i in 0 until 100){ //모의 데이터 100개.
             val crime = Crime()
             crime.title = "Crime #$i"
             crime.isSolved = i % 2 == 0
             crimes += crime //산술연산자로 리스트에 추가할 수 있다.
         }
-    }
+    }*/
+
+    private val crimeRepository = CrimeRepository.get()
+    val crimes = crimeRepository.getCrimes()
 }
---
+++ app/src/main/java/com/biginerdranch/android/criminalintent/database/CrimeDatabase.kt
@@ -0,0 +1,14 @@
+package com.biginerdranch.android.criminalintent.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.biginerdranch.android.criminalintent.Crime
+
+@Database (entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+
+    abstract fun crimeDao() : CrimeDao
+
+}
